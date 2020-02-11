985d06f30061e27caf5f8300976fafbbb771f922
bozhi <q576333@gmail.com>
2020-02-12 05:37:00 +0800

    Add generate line function
    To visualization, so I build generate line funtion in Curve_common class

---
+++ include/agv_path_smoothing/Curve_common.h
@@ -0,0 +1,14 @@
+#ifndef AGV_PATH_SMOOTHING_CURVE_COMMON_H_
+#define AGV_PATH_SMOOTHING_CURVE_COMMON_H_
+
+#include "geometry_msgs/Point.h"
+#include "nav_msgs/Path.h"
+
+class Curve_common
+{
+    public:
+        Curve_common();
+        nav_msgs::Path Generate_Line(geometry_msgs::Point start_point, geometry_msgs::Point end_point, double t_intervel);
+    private:
+}
+#endif //AGV_PATH_SMOOTHING_CURVE_COMMON_H_
---
+++ src/Curve_common.cpp
@@ -0,0 +1,31 @@
+#include "agv_path_smoothing/Curve_common.h";
+
+#include "geometry_msgs/PoseStamped.h"
+
+Curve_common::Curve_common()
+{
+
+}
+
+nav_msgs::Path Curve_common::Generate_Line(geometry_msgs::Point start_point, geometry_msgs::Point end_point, double t_intervel)
+{
+    nav_msgs::Path line_result;
+    geometry_msgs::PoseStamped current_pose;
+    current_pose.header.frame_id = "odom";
+
+    int segment = 1 / t_intervel;
+    double line_parameter = 0;
+
+    
+    for(int i = 0; i < segment; i++)
+    {
+        current_pose.header.seq = i;
+        current_pose.pose.position.x = start_point.x * (1 - line_parameter) + end_point.x * (line_parameter);
+        current_pose.pose.position.y = start_point.y * (1 - line_parameter) + end_point.y * (line_parameter);
+
+        line_result.poses.push_back(current_pose);
+        line_parameter += t_intervel;
+    }
+
+    return line_result;
+}
