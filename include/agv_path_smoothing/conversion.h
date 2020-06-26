4e507464e9c92138a670b12bab34be4946824baf
bozhi <q576333@gmail.com>
2020-06-26 13:12:40 +0800

    Adding Overload EigenTrajectoryVectorFromVector funtion support geometry_msgs::Point type

--- include/agv_path_smoothing/conversion.h
+++ include/agv_path_smoothing/conversion.h
@@ -12,28 +12,31 @@ inline Eigen::Vector3d EigenVecter3dFromPointMsg(const geometry_msgs::Point& msg
 
 inline EigenTrajectoryPoint::Vector EigenTrajectoryVectorFromVector(const std::vector<geometry_msgs::PoseStamped> &plan)
 {
-  double x = 0;
-  double y = 0;
   EigenTrajectoryPoint::Vector eigen_trajectory_point_vec;
-  
   EigenTrajectoryPoint eigen_trajectory_point;
-  Eigen::Vector3d point_vector;
-  //eigen_trajectory_point_vec.resize(plan.size());
-  std::cout << "size of plan vector :" << plan.size() << "\n";
+
   for(int i = 0; i < plan.size(); i++)
   {
-    x = plan.at(i).pose.position.x;
-    y = plan.at(i).pose.position.y;
-    point_vector(0) = x;
-    point_vector(1) = y;
-    eigen_trajectory_point.position = point_vector;
+    eigen_trajectory_point.position(0) = plan.at(i).pose.position.x;
+    eigen_trajectory_point.position(1) = plan.at(i).pose.position.y;
     eigen_trajectory_point_vec.push_back(eigen_trajectory_point);
   }
-  // for(int i = 0; i < eigen_trajectory_point_vec.size(); i++)
-  // {
-  //     std::cout << i << "'s eigen_trajectory_point_vec in path smoothing x : " << eigen_trajectory_point_vec.at(i).position(0) << "\n";
-  //     std::cout << i << "'s eigen_trajectory_point_vec in path smoothing y : " << eigen_trajectory_point_vec.at(i).position(1) << "\n";
-  // }
+
+  return eigen_trajectory_point_vec;
+}
+
+inline EigenTrajectoryPoint::Vector EigenTrajectoryVectorFromVector(const std::vector<geometry_msgs::Point> &discreate_point_vec)
+{
+  EigenTrajectoryPoint::Vector eigen_trajectory_point_vec;
+  EigenTrajectoryPoint eigen_trajectory_point;
+
+  for(int i = 0; i < discreate_point_vec.size(); i++)
+  {
+    eigen_trajectory_point.position(0) = discreate_point_vec.at(i).x;
+    eigen_trajectory_point.position(1) = discreate_point_vec.at(i).y;
+    eigen_trajectory_point_vec.push_back(eigen_trajectory_point);
+  }
+  
   return eigen_trajectory_point_vec;
 }
 
